0757ce2d5def067edfff3e118bf8bfd0e12a0660
Erjon001 <erjonrrapi429@gmail.com>
2024-09-25 11:19:31 +0200

    Comment

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ def center_label(label, window_width):
     label_width = label.winfo_reqwidth()
     x_position = (window_width - label_width) // 2
     return x_position
-
+#comment
 # Function to highlight labels on hover
 def highlight_label(label):
     original_color = label.cget("bg")
