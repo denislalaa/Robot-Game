cd1d20c6c6429ce17f159412b52322be43708f99
Danjel004 <dramku22@epoka.edu.al>
2024-09-26 10:10:40 +0200

    Danjeli D

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
 import turtle
 import pygame
 import player
-# Ensure this is your player.py file with Player and Barrier classes
 from tkinter import *
-
-
 
 volume_muted = False
 
@@ -37,9 +34,10 @@ def start_game():
         'right': 360,
     }
 
-    # Create the barrier
+    # Create the barrier and door
     barrier1 = player.Barrier(gif_file=r".\\assets\\electric_barrier.gif", position=(0, -200))
-    door_=player.Barrier(gif_file=r".\\assets\\door_.gif",position=(330, -155))
+    door_ = player.Barrier(gif_file=r".\\assets\\door_.gif", position=(330, -155))
+
     # Create maze (optional)
     maze = turtle.Turtle()
     maze.penup()
@@ -58,25 +56,37 @@ def start_game():
 
     # Set up keyboard bindings
     sc.listen()
-    sc.onkey(robot.go_left, "Left")  # Move left
-    sc.onkey(robot.go_right, "Right")  # Move right
-    sc.onkey(robot.jump, "space")  # Jump when spacebar is pressed
+    sc.onkeypress(robot.go_left, "Left")  # Move left
+    sc.onkeypress(robot.go_right, "Right")  # Move right
+    sc.onkeypress(robot.jump, "space")  # Jump when spacebar is pressed
 
     # Start the gravity and jump updates
-
     robot.update_jump()
 
+    # Function to move to level 2
+    def go_to_level_2():
+        sc.clearscreen()
+        sc.bgcolor("lightblue")
+        sc.title("Niveli 2")
+        print("Kaloi në nivelin 2")
+
+    # Function to check if robot has reached the door
+    def check_door():
+        if robot.check_collision(door_):
+            print("Roboti arriti te dera")
+            go_to_level_2()
+
+    # Function to check collisions
     def check_collisions():
         if robot.check_collision(barrier1):
             print("Level Failed")
             robot.t.goto(-330, -190)  # Restart the level on collision
-
+        else:
+            check_door()  # Check if robot has reached the door
 
         turtle.Screen().ontimer(check_collisions, 100)
 
     check_collisions()
-
-
 
 # Function to open a new window for options
 def open_options():
@@ -100,7 +110,6 @@ def exit_game():
     turtle.bye()  # Close the turtle window
     window.quit()  # Close the Tkinter window
 
-
 def volume_down():
     global volume_muted
     if volume_muted:
@@ -109,28 +118,14 @@ def volume_down():
     else:
         pygame.mixer.music.set_volume(0.0)  # Ulim volumin
         volume_muted = True
-def go_to_level_2():
-    sc=turtle.Screen()
-    sc.clearscreen()
-    sc.bgcolor("lightblue")
-    sc.title("Niveli 2 ")
-
-def check_door(roboti, door_):
-    if roboti.distance(door_.t) < 20:
-        print("Roboti arriti deren")
-        go_to_level_2()
 
 # Create the main window
 window = Tk()
 window.title("Circuit Runner")
 window.geometry('800x800')
 
-
-
 # Bind the Escape key to exit fullscreen mode
 window.bind("<Escape>", lambda e: window.attributes("-fullscreen", False))
-
-
 
 # Ngarkojmë foton e sfondit
 background_image = PhotoImage(file="background.png")
@@ -139,8 +134,8 @@ background_label.place(x=0, y=0, relwidth=1, relheight=1)
 
 # Ngarkojmë ikonën e muzikës dhe e vendosim në këndin e siperm djathtas
 music_image = PhotoImage(file="music.png")
-music_button = Button(window, image=music_image, bg='#1f3659')  # Mund të vendosësh sfondin për të kombinuar me dritaren
-music_button.place(x=700, y=50)  # Pozicionohet në këndin e poshtëm djathtas
+music_button = Button(window, image=music_image, bg='#1f3659')
+music_button.place(x=700, y=50)
 music_button.bind('<Enter>', lambda e: highlight_label(music_button))
 music_button.bind('<Button-1>', lambda e: volume_down())
 
@@ -152,7 +147,6 @@ pygame.mixer.music.play(-1)
 # Update window size
 window.update()
 window_width = window.winfo_width()
-
 
 # Create buttons and bind functions
 button2 = Button(window, text="Start", font=("Comic Sans MS", 28), bg='#1f3659', fg='black', compound="center", command=start_game)
