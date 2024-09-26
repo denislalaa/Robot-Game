5ec8bf5a83f5db13526b153c098ab6ecf70b67f3
denislalaa <dlala22@epoka.edu.al>
2024-09-26 11:15:20 +0200

    Option mode

--- level3.py
+++ level3.py
@@ -1,7 +1,10 @@
-import turtle
-import pygame
-import player
-from tkinter import *
+# Function to move to level 3
+def go_to_level_3():
+    global level
+    level = 3  # Update level to 3
+    sc.clearscreen()  # Clear the screen for the new level
+    sc.bgcolor("lightgreen")  # Set a different background color for level 3
+    sc.title("Niveli 3")  # Set the title for level 3
 
 
-#
+
