a129f3c277577c6298c8bf8e059961cd5e1ab536
denislalaa <dlala22@epoka.edu.al>
2024-09-26 11:22:22 +0200

    Option mode

--- level3.py
+++ level3.py
@@ -3,25 +3,57 @@ import pygame
 import player
 from tkinter import *
 
-# Function to move to level 3
-def go_to_level_3(sc=None, boundaries=None):
+# Function to move to the next level (Level 3)
+def go_to_level_3():
     global level
     level = 3  # Update level to 3
-    sc.clearscreen()  # Clear the screen for the new level
-    sc.bgcolor("lightgreen")  # Set a different background color for level 3
-    sc.title("Niveli 3")  # Set the title for level 3
+    sc.clearscreen()
+    sc.bgcolor("lightgreen")  # Change background color for Level 3
+    sc.title("Niveli 3")  # Update title for Level 3
 
- # Krijimi i barrierave per nivelin 3
-    barrier2 = player.Barrier(gif_file=r".\\assets\\new_barrier.gif", position=(0, -100))  # Example barrier
-    door_3 = player.Barrier(gif_file=r".\\assets\\door_3.gif", position=(330, -155))  # New door for level 3
+    # Create a turtle for drawing boundaries
+    maze = turtle.Turtle()
+    maze.penup()
+    maze.pensize(2)
 
-    # Create and position robot for level 3
+    # Draw the outer rectangle for the first floor
+    maze.goto(360, 210)
+    maze.pendown()
+    maze.goto(360, -210)
+    maze.goto(-360, -210)
+    maze.goto(-360, 210)
+    maze.goto(360, 210)
+
+    # Draw the horizontal line for the first floor
+    maze.penup()
+    maze.goto(-360, 0)  # Starting point of the line
+    maze.pendown()
+    maze.goto(360, 0)  # End point of the line
+
+    # Draw the horizontal line for the second floor
+    maze.penup()
+    maze.goto(-360, 140)  # Starting point of the line for the second floor
+    maze.pendown()
+    maze.goto(360, 140)  # End point of the line for the second floor
+
+    # Draw the horizontal line for the third floor
+    maze.penup()
+    maze.goto(-360, 280)  # Starting point of the line for the third floor
+    maze.pendown()
+    maze.goto(360, 280)  # End point of the line for the third floor
+
+    maze.hideturtle()  # Hide the turtle after drawing
+
+    # Reposition the robot for level 3
     robot = player.Player(gif_file=r".\\assets\\roboti.gif", boundaries=boundaries)
-    robot.t.goto(-330, -190)  # Set robot's starting position for level 3
+    robot.t.goto(-330, 270)  # Set robot's starting position at the third floor
 
-    # Set up keyboard bindings for level 3
+    # Set up keyboard bindings
     sc.listen()
-    sc.onkeypress(robot.go_left, "Left")
-    sc.onkeypress(robot.go_right, "Right")
-    sc.onkeypress(robot.jump, "space")
+    sc.onkeypress(robot.go_left, "Left")  # Move left
+    sc.onkeypress(robot.go_right, "Right")  # Move right
+    sc.onkeypress(robot.jump, "space")  # Jump when spacebar is pressed
+
+    # Start the gravity and jump updates
+    robot.update_jump()  # Ensure the jump function is called
 
